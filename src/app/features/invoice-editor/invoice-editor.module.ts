c0979fd6c14d42ac49421721049bad24e1f15621
Paolo Laghetto <paolo.laghetto@gmail.com>
2023-02-16 19:33:25 +0100

    create modules

---
+++ src/app/features/invoice-editor/invoice-editor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InvoiceEditorRoutingModule } from './invoice-editor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InvoiceEditorRoutingModule
+  ]
+})
+export class InvoiceEditorModule { }
